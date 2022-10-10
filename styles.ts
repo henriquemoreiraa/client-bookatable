ab9e2b0ae56a48cebaeccbe135007a8fccf8cd7b
Henrique Moreira <riqimoreiraa@gmail.com>
2022-10-10 17:37:50 -0300

    first commit

---
+++ styles.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  display: flex;
+  justify-content: space-between;
+  background-color: red;
+  height: 100%;
+`;
+export const All = styled.View`
+  /* background-color: red; */
+`;
+export const TitleButton = styled.View`
+  /* background-color: red; */
+`;
+
+export const Title = styled.Text`
+  color: #fff;
+  font-size: 40px;
+`;
+
+export const Yellow = styled.View`
+  background-color: #000;
+`;
