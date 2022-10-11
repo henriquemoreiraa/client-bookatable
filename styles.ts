813ac281463392249661d2f70f56437f4bd2534a
Henrique Moreira <riqimoreiraa@gmail.com>
2022-10-11 16:04:09 -0300

    main screen

--- styles.ts
+++ styles.ts
@@ -1,23 +1,49 @@
 import styled from "styled-components/native";
 
+const black = "#292727";
+
 export const Container = styled.View`
   display: flex;
   justify-content: space-between;
-  background-color: red;
+  align-items: center;
+  background-color: #ededea;
   height: 100%;
 `;
-export const All = styled.View`
-  /* background-color: red; */
-`;
+
 export const TitleButton = styled.View`
-  /* background-color: red; */
+  display: flex;
+  align-items: center;
 `;
 
 export const Title = styled.Text`
-  color: #fff;
-  font-size: 40px;
+  font-size: 92px;
+  text-align: center;
+  font-family: "DMSERIF";
+  line-height: 93px;
+  color: ${black};
 `;
 
 export const Yellow = styled.View`
-  background-color: #000;
+  background-color: #fff6b6;
+  height: 450px;
+  width: 450px;
+  border-radius: 500;
+  position: absolute;
+  top: -150;
+  left: -55;
+  elevation: 5;
+  shadowcolor: "black";
+  shadowopacity: 0.5;
+  shadowradius: 10;
+`;
+
+export const Button = styled.Pressable`
+  background-color: ${black};
+  border-radius: 59px;
+  width: 198px;
+  height: 57px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
 `;
